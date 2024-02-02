b0b7f9808af3821cdfa6c603261ae9732c267261
nilton-manjate <ntnmanjate1@gmail.com>
2024-02-02 09:03:25 +0200

    add different error msg when the user submit empty input

--- error.js
+++ error.js
@@ -8,14 +8,18 @@ const form__error = document.getElementsByClassName("form__error")[0];
 
 function validate() {
     if (form__input.checkValidity()) {
-        form__error.style.display = "none";
         form__input.value = "";
         form__input.style.borderColor = "var(--pale-blue)";
+        form__error.innerHTML = "";
 
         alert("Email sent successfully")
+    } else if (form__input.value == "") {
+        form__input.style.borderColor = "var(--light-red)";
+        form__error.innerHTML = "Whoops! It looks like you forgot to add your email";
+        form__input.value = "example@email.com";
     } else {
         form__input.style.borderColor = "var(--light-red)";
-        form__error.style.display = "block";
+        form__error.innerHTML = "Please provide a valid email address";
         form__input.value = "example@email.com";
     }
 };
